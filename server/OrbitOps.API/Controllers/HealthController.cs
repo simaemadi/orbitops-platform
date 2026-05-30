fe4a06231d3770cf8b39f5e6d55106783a3863f9
simaemadi <emadi.sima@gmail.com>
2026-05-30 15:41:31 +0330

    chore(api): add Controllers folder

---
+++ server/OrbitOps.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrbitOps.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("OrbitOps API is running");
+        }
+    }
+}
